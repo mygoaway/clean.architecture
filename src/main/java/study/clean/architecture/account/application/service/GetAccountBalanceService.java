4f05a2a81736f8485d5099ad11727e44394c6556
mygoaway <mygoaway2@gmail.com>
2023-07-16 22:33:10 +0900

    불필요한 import 삭제

--- src/main/java/study/clean/architecture/account/application/service/GetAccountBalanceService.java
+++ src/main/java/study/clean/architecture/account/application/service/GetAccountBalanceService.java
@@ -3,7 +3,6 @@ package study.clean.architecture.account.application.service;
 import lombok.RequiredArgsConstructor;
 import study.clean.architecture.account.application.port.in.GetAccountBalanceQuery;
 import study.clean.architecture.account.application.port.out.LoadAccountPort;
-import study.clean.architecture.account.domain.Account;
 import study.clean.architecture.account.domain.Account.AccountId;
 import study.clean.architecture.account.domain.Money;
 
